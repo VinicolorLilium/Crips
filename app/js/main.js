f101aee4f88974f50406f544b5dfd3a56a4baa19
Tania <vinicolorlilium@yandex.ru>
2025-08-08 14:14:20 +0500

    доработана страница каталога, сделан фильтр по цене, добавлены карточки товаров и банер, сделаны списки сортировок

--- app/js/main.js
+++ app/js/main.js
@@ -51,3 +51,51 @@ if (filterBtns ) {
     });
   });
 }
+
+const rangeMin = document.getElementById("range-min");
+const rangeMax = document.getElementById("range-max");
+const minLabel = document.getElementById("min-price-label");
+const maxLabel = document.getElementById("max-price-label");
+const track = document.querySelector(".filter__range-track");
+
+if (rangeMin && rangeMax && minLabel && maxLabel && track) {
+  const minGap = 1; // минимальный зазор
+
+  function updateTrack() {
+    let minVal = parseInt(rangeMin.value);
+    let maxVal = parseInt(rangeMax.value);
+
+    if (maxVal - minVal < minGap) {
+      if (this.id === "range-min") {
+        rangeMin.value = maxVal - minGap;
+      } else {
+        rangeMax.value = minVal + minGap;
+      }
+      minVal = parseInt(rangeMin.value);
+      maxVal = parseInt(rangeMax.value);
+    }
+
+    minLabel.textContent = `${minVal}.00 EUR`;
+    maxLabel.textContent = `${maxVal}.00 EUR`;
+
+    const range = parseInt(rangeMax.max) - parseInt(rangeMin.min);
+    const left = ((minVal - rangeMin.min) / range) * 100;
+    const right = 100 - ((maxVal - rangeMax.min) / range) * 100;
+
+    track.style.background = `linear-gradient(
+      to right,
+      lightgray ${left}%,
+      black ${left}%,
+      black ${100 - right}%,
+      lightgray ${100 - right}%
+    )`;
+  }
+
+  rangeMin.addEventListener("input", updateTrack);
+  rangeMax.addEventListener("input", updateTrack);
+
+  // Отрисовка при загрузке с дефолтными значениями из HTML
+  updateTrack();
+}
+
+
